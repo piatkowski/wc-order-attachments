57b762c451dd273aa2489e66934a2fece8897020
Krzysztof <piatkowski92@gmail.com>
2022-12-28 00:15:46 +0100

    Initial commit

---
+++ wc-order-attachments.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+Plugin Name: Order Attachments for WooCommerce
+Description: Plugin allows to add attachments to orders in WooCommerce. Files are protected and available for download by the buyer.
+Version: 1.0.0
+Author: Krzysztof Piątkowski
+License: GPLv2
+*/
